793dc9b50e3432dcb0422a1bcd41da69146adb24
karolmartg <karolmartgamb@hotmail.com>
2025-01-28 18:38:20 -0600

    Commit inicial
    Commit inicial - Semana 3

---
+++ KN_ProyectoClase/Controllers/PersonasController.cs
@@ -0,0 +1,28 @@
+﻿using KN_ProyectoClase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_ProyectoClase.Controllers
+{
+    public class PersonasController : Controller
+    {
+        [HttpGet]
+        public ActionResult RegistrarPersona()
+        {
+            var personasModelo = new PersonasModel();
+            personasModelo.Nombre = "EDUARDO";
+
+            return View(personasModelo);
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarPersona(PersonasModel modelo)
+        {
+            return View();
+        }
+
+    }
+}
